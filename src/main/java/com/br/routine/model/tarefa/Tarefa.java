feaeee95ec811fda7ee12d1064e8657761f37016
Gabriel de Carvalho Antunes <gabrielca.pro@gmail.com>
2026-01-05 17:18:24 -0300

    feat: Removido o uso do lombok

--- src/main/java/com/br/routine/model/tarefa/Tarefa.java
+++ src/main/java/com/br/routine/model/tarefa/Tarefa.java
@@ -1,9 +1,12 @@
 package com.br.routine.model.tarefa;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
-@Entity(name = "Tarefa")
+@Entity(name = "tarefa")
 @Table(name = "tarefa")
+@Getter
 public class Tarefa {
 
     @Id
@@ -13,4 +16,19 @@ public class Tarefa {
     private String descricao;
     private boolean ativa;
 
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public boolean isAtiva() {
+        return ativa;
+    }
 }
