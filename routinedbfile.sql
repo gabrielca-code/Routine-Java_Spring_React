30131390cd2e220211f7a28966dc4fe3d76b271e
Gabriel de Carvalho Antunes <gabrielca.pro@gmail.com>
2026-01-05 16:56:57 -0300

    feat: Banco de dados do sistema adicionado

---
+++ routinedbfile.sql
@@ -0,0 +1,12 @@
+create database if not exists routinedb;
+use routinedb;
+
+create table if not exists tarefa(
+	id int primary key auto_increment,
+    titulo varchar(255) not null,
+    descricao text(500),
+    ativa boolean default(true)
+);
+
+insert into tarefa(titulo, descricao) values ('Teste1', 'Teste1'), ('Teste2', 'Teste2');
+select * from tarefa;
