a69d7ba2b2485f01d616ac0da67f053a8d0c39e8
Gabriel de Carvalho Antunes <gabrielca.pro@gmail.com>
2026-01-05 17:27:52 -0300

    fix: Get corretamente funcionando

--- src/main/java/com/br/routine/controller/TarefaController.java
+++ src/main/java/com/br/routine/controller/TarefaController.java
@@ -1,11 +1,11 @@
 package com.br.routine.controller;
 
 import com.br.routine.model.tarefa.Tarefa;
+import com.br.routine.model.tarefa.TarefaAdicionarDTO;
+import com.br.routine.model.tarefa.TarefaListagemDTO;
 import com.br.routine.repository.TarefaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,12 +17,17 @@ public class TarefaController {
     private TarefaRepository tarefaRepository;
 
     @GetMapping
-    public List<Tarefa> getAll() {
-        List<Tarefa> tarefas = tarefaRepository.findAll();
+    public List<TarefaListagemDTO> getAll() {
+        List<TarefaListagemDTO> tarefas = tarefaRepository.findAll().stream().map(TarefaListagemDTO::new).toList();
 
         System.out.println(tarefas);
 
         return tarefas;
+    }
+
+    @PostMapping
+    public void addTarefa(@RequestBody TarefaAdicionarDTO dados) {
+
     }
 
 }
