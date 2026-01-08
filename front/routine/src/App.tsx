0781301ec336998b38da6fc4275b2c37f0d89693
Gabriel de Carvalho Antunes <gabrielca.pro@gmail.com>
2026-01-07 21:51:41 -0300

    feat: Ajustado tela que receberá os dados de reposta da API

--- front/routine/src/App.tsx
+++ front/routine/src/App.tsx
@@ -1,18 +1,15 @@
-//import { useState } from 'react'
-//import reactLogo from './assets/react.svg'
-//import viteLogo from '/vite.svg'
 import './App.css'
 import { Card } from './components/card/card';
-import type { TarefaData } from './interface/tarefaData';
+import { useTarefaData } from './hooks/useTarefasData';
 
 function App() {
-  const tarefaData: TarefaData[] = [];
+  const { data } = useTarefaData();
 
   return (
     <div className='container'>
       <h1>Tarefas</h1>
       <div className='card-grid'>
-        {tarefaData.map(tarefa => <Card id={tarefa.id} titulo={tarefa.titulo} descricao={tarefa.descricao}/>)}
+        {data?.map(tarefa => <Card id={tarefa.id} titulo={tarefa.titulo} descricao={tarefa.descricao}/>)}
       </div>
     </div>
   )
