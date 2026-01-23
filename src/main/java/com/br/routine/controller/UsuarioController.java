d5e4613db5773f426f38f64db6e1940e2cc26b8b
Gabriel de Carvalho Antunes <gabrielca.pro@gmail.com>
2026-01-22 23:37:32 -0300

    feat: getMapping usuario

--- src/main/java/com/br/routine/controller/UsuarioController.java
+++ src/main/java/com/br/routine/controller/UsuarioController.java
@@ -1,9 +1,14 @@
 package com.br.routine.controller;
 
+import com.br.routine.model.usuario.UsuarioListagemDTO;
 import com.br.routine.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/usuario")
@@ -11,5 +16,11 @@ public class UsuarioController {
 
     @Autowired
     private UsuarioRepository usuarioRepository;
+
+    @GetMapping("/{id}")
+    public UsuarioListagemDTO getUsuario(@PathVariable Long id) {
+        var usuario = usuarioRepository.getReferenceById(id);
+        return new UsuarioListagemDTO(usuario);
+    }
 
 }
