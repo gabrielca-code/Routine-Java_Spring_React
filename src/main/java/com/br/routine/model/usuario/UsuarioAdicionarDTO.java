4e0b6da40c8c0ef6ff93f77afaecb7704be3ab6e
Gabriel de Carvalho Antunes <gabrielca.pro@gmail.com>
2026-01-23 23:25:04 -0300

    feat: validando add usuario dto

--- src/main/java/com/br/routine/model/usuario/UsuarioAdicionarDTO.java
+++ src/main/java/com/br/routine/model/usuario/UsuarioAdicionarDTO.java
@@ -1,7 +1,11 @@
 package com.br.routine.model.usuario;
 
+import javax.validation.constraints.NotBlank;
+
 public record UsuarioAdicionarDTO(
+        @NotBlank
         String login,
+        @NotBlank
         String senha
 ) {
 }
