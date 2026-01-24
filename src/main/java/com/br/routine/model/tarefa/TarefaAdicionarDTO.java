e364c9203fbf6ea0f7eaa1bec759b0ce5622f607
Gabriel de Carvalho Antunes <gabrielca.pro@gmail.com>
2026-01-23 23:23:43 -0300

    feat: adicionado dependencia de validation no projeto

--- src/main/java/com/br/routine/model/tarefa/TarefaAdicionarDTO.java
+++ src/main/java/com/br/routine/model/tarefa/TarefaAdicionarDTO.java
@@ -1,6 +1,9 @@
 package com.br.routine.model.tarefa;
 
+import org.antlr.v4.runtime.misc.NotNull;
+
 public record TarefaAdicionarDTO(
+
         String titulo,
         String descricao
         ) {
