8a5b44388d7029b76c512f487f6332c3ba976bec
Gabriel de Carvalho Antunes <gabrielca.pro@gmail.com>
2026-01-26 16:16:06 -0300

    feat: Subtarefa repository criado

---
+++ src/main/java/com/br/routine/repository/SubtarefaRepository.java
@@ -0,0 +1,7 @@
+package com.br.routine.repository;
+
+import com.br.routine.model.subtarefa.Subtarefa;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SubtarefaRepository extends JpaRepository<Subtarefa, Long> {
+}
