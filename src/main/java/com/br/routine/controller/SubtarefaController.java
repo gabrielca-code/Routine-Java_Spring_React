cd166865eca700f03db40bd73195f2cb807ce66d
Gabriel de Carvalho Antunes <gabrielca.pro@gmail.com>
2026-01-25 23:47:21 -0300

    feat: subtarefa controller criada

---
+++ src/main/java/com/br/routine/controller/SubtarefaController.java
@@ -0,0 +1,10 @@
+package com.br.routine.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/subtarefa")
+public class SubtarefaController {
+
+}
