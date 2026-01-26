400d7f4f5486255aea53726d75e257cd7a13a99b
Gabriel de Carvalho Antunes <gabrielca.pro@gmail.com>
2026-01-26 16:20:21 -0300

    feat: Subtarefa Post mapping

--- src/main/java/com/br/routine/controller/SubtarefaController.java
+++ src/main/java/com/br/routine/controller/SubtarefaController.java
@@ -3,9 +3,7 @@ package com.br.routine.controller;
 import com.br.routine.model.subtarefa.Subtarefa;
 import com.br.routine.repository.SubtarefaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,6 +17,11 @@ public class SubtarefaController {
     @GetMapping
     public List<Subtarefa> listAllSubtarefas() {
         return subtarefaRepository.findAll();
+    }
+
+    @PostMapping
+    public void addSubtarefa(@RequestBody Subtarefa subtarefa) {
+        subtarefaRepository.save(subtarefa);
     }
 
 }
