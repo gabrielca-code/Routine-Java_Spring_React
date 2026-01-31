efe7030bc65b3dbd4b81e14e6387411acabe47d2
Gabriel de Carvalho Antunes <gabrielca.pro@gmail.com>
2026-01-30 23:48:26 -0300

    feat: Response Entities para tarefa controller

--- src/main/java/com/br/routine/controller/TarefaController.java
+++ src/main/java/com/br/routine/controller/TarefaController.java
@@ -5,8 +5,11 @@ import com.br.routine.model.tarefa.TarefaAdicionarDTO;
 import com.br.routine.model.tarefa.TarefaEditarDTO;
 import com.br.routine.model.tarefa.TarefaListagemDTO;
 import com.br.routine.repository.TarefaRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import javax.validation.Valid;
 import java.util.List;
@@ -27,27 +30,31 @@ public class TarefaController {
     }
 
     @PostMapping
-    public void adicionarTarefa(@RequestBody @Valid TarefaAdicionarDTO dados) {
+    @Transactional
+    public ResponseEntity adicionarTarefa(@RequestBody @Valid TarefaAdicionarDTO dados, UriComponentsBuilder uriBuilder) {
         var tarefa = new Tarefa(dados);
-
         tarefaRepository.save(tarefa);
 
-        return;
+        var uri = uriBuilder.path("/tarefa/{id}").buildAndExpand(tarefa.getId()).toUri();
+
+        return ResponseEntity.created(uri).body(new TarefaListagemDTO(tarefa));
     }
 
     @PutMapping
-    public void editarTarefa(@RequestBody TarefaEditarDTO dados) {
+    @Transactional
+    public ResponseEntity editarTarefa(@RequestBody TarefaEditarDTO dados) {
         var tarefa = tarefaRepository.getReferenceById(dados.id());
         tarefa.editar(dados);
 
-        return;
+        return ResponseEntity.ok(new TarefaListagemDTO(tarefa));
     }
 
     @DeleteMapping("/{id}")
-    public void removerTarefa(@PathVariable Long id) {
+    @Transactional
+    public ResponseEntity removerTarefa(@PathVariable Long id) {
         tarefaRepository.deleteById(id); //remoção física
 
-        return;
+        return ResponseEntity.noContent().build();
     }
 
 }
