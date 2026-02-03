c797e829e808aa02fdac71d9198c99d35feb5397
Gabriel de Carvalho Antunes <gabrielca.pro@gmail.com>
2026-02-02 23:22:17 -0300

    feat: Banco tarefa alterado

--- routinedbfile.sql
+++ routinedbfile.sql
@@ -5,8 +5,10 @@ create table if not exists tarefa(
 	id int primary key auto_increment,
     titulo varchar(255) not null,
     descricao text(500),
-    ativa boolean default(true)
+    ativa boolean default true
 );
+
+
 
 insert into tarefa(titulo, descricao) values ('Teste1', 'Teste1'), ('Teste2', 'Teste2');
 select * from tarefa;
