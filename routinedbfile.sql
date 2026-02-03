695ad64c4d6e6eb42e865c866b3d5950927b8abb
Gabriel de Carvalho Antunes <gabrielca.pro@gmail.com>
2026-02-02 23:34:05 -0300

    feat: Banco tarefa alterado para ser muitos para um com usuário

--- routinedbfile.sql
+++ routinedbfile.sql
@@ -14,7 +14,9 @@ create table if not exists tarefa(
 	id int primary key auto_increment,
     titulo varchar(255) not null,
     descricao text(500),
-    ativa boolean default true
+    ativa boolean default true,
+    idUsuario int not null,
+    foreign key (idUsuario) references usuario (id)
 );
 
 create table if not exists subtarefa(
@@ -28,14 +30,16 @@ insert into usuario(login, senha, nome) values
 ('admin', 'admin', 'Usuario 1'),
 ('login', 'senha', 'Usuario 2');
 
-insert into tarefa(titulo, descricao) values
-('Teste1', 'Teste1'), 
-('Teste2', 'Teste2');
+insert into tarefa(titulo, descricao, idUsuario) values
+('Teste1', 'Teste1', 1), 
+('Teste2', 'Teste2', 1),
+('Teste3', 'Teste3', 2);
 select * from tarefa;
 
 insert into subtarefa(titulo, idTarefa) values
 ('Subtarefa 1', 1),
 ('Subtarefa 2', 1),
 ('Subtarefa 3', 1),
-('Subtarefa 4', 2);
+('Subtarefa 4', 2),
+('Subtarefa 5', 3);
 select * from subtarefa;
