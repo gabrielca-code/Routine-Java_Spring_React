89d3716f6dd193689d9009c51b21f88398dd3397
Gabriel de Carvalho Antunes <gabrielca.pro@gmail.com>
2026-02-04 11:33:57 -0300

    feat: Adicionando usuário na listagem de tarefa

--- src/main/java/com/br/routine/model/tarefa/TarefaListagemDTO.java
+++ src/main/java/com/br/routine/model/tarefa/TarefaListagemDTO.java
@@ -1,14 +1,16 @@
 package com.br.routine.model.tarefa;
 
+import com.br.routine.model.usuario.Usuario;
+
 public record TarefaListagemDTO(
         Long id,
         String titulo,
         String descricao,
-        //Adicionar usuario
+        Usuario usuario,
         Boolean ativa
 ) {
 
     public TarefaListagemDTO(Tarefa tarefa) {
-        this(tarefa.getId(), tarefa.getTitulo(), tarefa.getDescricao(), tarefa.isAtiva());
+        this(tarefa.getId(), tarefa.getTitulo(), tarefa.getDescricao(), tarefa.getUsuario(), tarefa.isAtiva());
     }
 }
