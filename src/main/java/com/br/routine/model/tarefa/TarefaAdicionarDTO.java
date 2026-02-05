71cafbd242eebcf4a5e79c5e3f24a9382d001cd1
Gabriel de Carvalho Antunes <gabrielca.pro@gmail.com>
2026-02-05 16:10:44 -0300

    fix: Validando not null em tarefa

--- src/main/java/com/br/routine/model/tarefa/TarefaAdicionarDTO.java
+++ src/main/java/com/br/routine/model/tarefa/TarefaAdicionarDTO.java
@@ -1,14 +1,15 @@
 package com.br.routine.model.tarefa;
 
 import com.br.routine.model.usuario.Usuario;
-import org.antlr.v4.runtime.misc.NotNull;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 public record TarefaAdicionarDTO(
         @NotBlank
         String titulo,
         String descricao,
+        @NotNull
         Usuario usuario
         ) {
 }
