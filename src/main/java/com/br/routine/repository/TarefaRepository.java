d7b4de41046daf2a6e43241b7ec6929ce4399882
Gabriel de Carvalho Antunes <gabrielca.pro@gmail.com>
2026-02-06 23:44:20 -0300

    feat: Criando query para obter somente tarefas ativas

--- src/main/java/com/br/routine/repository/TarefaRepository.java
+++ src/main/java/com/br/routine/repository/TarefaRepository.java
@@ -2,5 +2,13 @@ package com.br.routine.repository;
 
 import com.br.routine.model.tarefa.Tarefa;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
-public interface TarefaRepository extends JpaRepository<Tarefa, Long> { }
+import java.util.List;
+
+public interface TarefaRepository extends JpaRepository<Tarefa, Long> {
+
+    @Query("SELECT * FROM tarefa t WHERE t.ativa = 1")
+    List<Tarefa> obterTarefasAtivas();
+
+}
