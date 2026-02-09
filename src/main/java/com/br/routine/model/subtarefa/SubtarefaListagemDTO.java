62df2f0039e95ef261c63fd40d5af010827ec31c
Gabriel de Carvalho Antunes <gabrielca.pro@gmail.com>
2026-02-08 21:54:15 -0300

    fix: Alterando get subtarefa para mostrar o DTO de tarefa

--- src/main/java/com/br/routine/model/subtarefa/SubtarefaListagemDTO.java
+++ src/main/java/com/br/routine/model/subtarefa/SubtarefaListagemDTO.java
@@ -1,15 +1,16 @@
 package com.br.routine.model.subtarefa;
 
 import com.br.routine.model.tarefa.Tarefa;
+import com.br.routine.model.tarefa.TarefaListagemDTO;
 
 public record SubtarefaListagemDTO(
         Long id,
         String titulo,
-        Tarefa tarefa
+        TarefaListagemDTO tarefa
 ) {
 
     public SubtarefaListagemDTO(Subtarefa subtarefa) {
-        this(subtarefa.getId(), subtarefa.getTitulo(), subtarefa.getTarefa());
+        this(subtarefa.getId(), subtarefa.getTitulo(), new TarefaListagemDTO(subtarefa.getTarefa()));
     }
 
 }
