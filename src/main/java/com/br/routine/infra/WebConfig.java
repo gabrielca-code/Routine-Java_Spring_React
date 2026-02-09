6f4a89b6261a1694c2663b64ac3e566ad9c70bdf
Gabriel de Carvalho Antunes <gabrielca.pro@gmail.com>
2026-02-09 20:01:57 -0300

    feat: Configurando cors

---
+++ src/main/java/com/br/routine/infra/WebConfig.java
@@ -0,0 +1,19 @@
+package com.br.routine.infra;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173")
+                .allowedMethods("DEL", "POST", "GET", "PUT")
+                .allowedHeaders("*")
+                .allowCredentials(false);
+    }
+
+}
