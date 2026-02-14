55609c756fff3a67596f91cbfd6253f7c128951f
Gabriel de Carvalho Antunes <gabrielca.pro@gmail.com>
2026-02-13 23:21:28 -0300

    feat: Criando rest controller advice

--- src/main/java/com/br/routine/infra/ExceptionHandling.java
+++ src/main/java/com/br/routine/infra/ExceptionHandling.java
@@ -1,10 +1,16 @@
 package com.br.routine.infra;
 
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class ExceptionHandling {
 
-
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity tratarErro404() {
+        return ResponseEntity.notFound().build();
+    }
 
 }
