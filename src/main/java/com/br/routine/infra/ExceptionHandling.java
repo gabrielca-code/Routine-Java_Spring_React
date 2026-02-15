dafcef615b337ecf11c272671b61fed2425570b1
Gabriel de Carvalho Antunes <gabrielca.pro@gmail.com>
2026-02-14 22:07:12 -0300

    feat: DTO para erro

--- src/main/java/com/br/routine/infra/ExceptionHandling.java
+++ src/main/java/com/br/routine/infra/ExceptionHandling.java
@@ -2,6 +2,7 @@ package com.br.routine.infra;
 
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -11,6 +12,14 @@ public class ExceptionHandling {
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity tratarErro404() {
         return ResponseEntity.notFound().build();
+    }
+
+
+
+    private record ErrosValidacaoDTO(String campo, String mensagem) {
+        public ErrosValidacaoDTO(FieldError erro) {
+            this(erro.getField(), erro.getDefaultMessage());
+        }
     }
 
 }
